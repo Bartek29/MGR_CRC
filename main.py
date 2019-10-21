0771474b872ed1a5296ab7e75823810950ee9141
Bartlomiej Chechlinski <bartek.chechlinski@gmail.com>
2019-10-21 20:00:19 +0200

    Add WiP model
    Signed-off-by: Bartlomiej Chechlinski <bartek.chechlinski@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
-from common.logger import Logger
-from generator.parser import Parser
+# from common.logger import Logger
+# from generator.parser import Parser
+# from PIL import Image
+#
+# Logger.set_level(Logger.INFO)
+# for i in range(2000):
+#     # Logger().error("Create {}/2000".format(i))
+#     p = Parser("schemas/h_capital_1.sch",
+#                move_point_rnd=2,
+#                paint_chance_rnd=0.8)
+#     p.parse()
+#     p.paint()
+#     img = Image.open('test.bmp')
+#
+#     new_img = img.resize((128, 128))
+#     new_img.save('database/gen_train/h_capital/train_{:04d}.png'.format(i), 'png')
+#
+# exit()
 
-Logger.set_level(Logger.DEBUG)
-Logger().info("Start")
-p = Parser("schemas/k_capital_1.sch",
-           move_point_rnd=2,
-           paint_chance_rnd=0.8)
-p.parse()
-p.paint()
+import tensorflow as tf  # deep learning library. Tensors are just multi-dimensional arrays
 
+datagen = tf.keras.preprocessing.image.ImageDataGenerator()
+train_it = datagen.flow_from_directory('database/real_train/',
+                                       batch_size=64)
+
+test_it = datagen.flow_from_directory('database/real_test/',
+                                      batch_size=64)
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit_generator(train_it, epochs=3, validation_steps=8)
+loss = model.evaluate_generator(test_it, steps=24)
+print(loss)
+# 2 classes:
+# gen [181.09806474049887, 0.8313802]
+# real [13.652371724446615, 0.99283856]
+# 10 classes:
+# gen [2512.6373901367188, 0.1953125]
+# real [2.1766373813152313, 0.2467448]
+model.save('crc.model')
